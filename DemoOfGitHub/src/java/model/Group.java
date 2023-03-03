40ee69076191cda6fe3b36ae91dce10fc6e64b94
Hoàng Nghĩa Hiệp <hoanghiep742003@gmail.com>
2023-03-03 22:41:26 +0700

    Add Getters & Setters P1
    Add Getters & Setters to Group, Major, Student and User

--- DemoOfGitHub/src/java/model/Group.java
+++ DemoOfGitHub/src/java/model/Group.java
@@ -4,10 +4,75 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Admin
  */
 public class Group {
+    private String grID;
+    private String grName;
+    private String courseID;
+    private String insID;
+    private ArrayList<Student> students = new ArrayList<>();
+    private ArrayList<Course> courses = new ArrayList<>();
+    private ArrayList<Instructor> instructors = new ArrayList<>();
+
+    public String getGrID() {
+        return grID;
+    }
+
+    public void setGrID(String grID) {
+        this.grID = grID;
+    }
+
+    public String getGrName() {
+        return grName;
+    }
+
+    public void setGrName(String grName) {
+        this.grName = grName;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getInsID() {
+        return insID;
+    }
+
+    public void setInsID(String insID) {
+        this.insID = insID;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
+
+    public ArrayList<Instructor> getInstructors() {
+        return instructors;
+    }
+
+    public void setInstructors(ArrayList<Instructor> instructors) {
+        this.instructors = instructors;
+    }
     
 }
---
+++ DemoOfGitHub/src/java/model/Major.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Major {
+    private int mid;
+    private String mname;
+
+    public int getMid() {
+        return mid;
+    }
+
+    public void setMid(int mid) {
+        this.mid = mid;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+    
+}
